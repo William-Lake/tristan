6c6ef5f9ec15c69ade712b241220a477514a2fcd
William Lake <N/A>
2019-08-30 07:58:57 -0600

    Disable time filter

--- main.py
+++ main.py
@@ -62,18 +62,20 @@ def gather_params():
     
     return subreddits, search_term
 
-@route('/tristan/<search_term>/<time_filter>/<subreddits>')
-def tristan(search_term,time_filter,subreddits):
+@route('/tristan/<search_term>/<subreddits>')
+def tristan(search_term,subreddits):
     
     logging.info('Received request.')
 
-    time_filter_options = ['all','day','hour','month','week','year']
+    time_filter = 'week'
 
-    if time_filter not in time_filter_options:
+    # time_filter_options = ['all','day','hour','month','week','year']
 
-        response.status = 400
+    # if time_filter not in time_filter_options:
 
-        return f'Time filter "{time_filter}" invalid. Options are: {", ".join(time_filter_options)}'
+    #     response.status = 400
+
+    #     return f'Time filter "{time_filter}" invalid. Options are: {", ".join(time_filter_options)}'
 
     # subreddits, search_term = gather_params()
 
